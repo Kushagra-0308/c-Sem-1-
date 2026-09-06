0e363c52d802391448d6951e051e9e9b6baf5106
Kushagra <kushagrajain2008@gmail.com>
2026-09-06 12:54:58 +0530

    learning c

---
+++ typecasting.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    float b = 5.5;
+    a= (int)b; // Typecasting float to int
+    printf("Value of a after typecasting: %d\n", a);
+    return 0;
+}
+    
